b446c4dba1f5a00137677bfafc755f954bfae031
itai chiriseri <itaineilchiriseri@gmail.com>
2023-08-09 11:20:02 +0200

    changed model on the home screen

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,7 +4,7 @@
     @include('layouts.navbars.guest.navbar')
     <main class="main-content  mt-0">
         <div class="page-header align-items-start min-vh-50 pt-5 pb-11 m-3 border-radius-lg"
-             style="background-image: url('img/bhebhiHeavy.jpg'); background-size: cover background-position: top;">
+             style="background-image: url('img/christina.png'); background-size: cover background-position: top;">
             <span class="mask bg-gradient-dark opacity-6"></span>
             <div class="container">
                 <div class="row justify-content-center">
@@ -19,12 +19,10 @@
             <div class="row mt-lg-n10 mt-md-n11 mt-n10 justify-content-center">
                 <div class="col-xl-4 col-lg-5 col-md-7 mx-auto">
                     <div class="card z-index-0">
-                        <div class="card-header text-center pt-4">
-                            <h5>Our Facebook Page</h5>
-                        </div>
+                       
                         <div class="row px-xl-5 px-sm-4 px-3">
                             <div class="col-12 ms-auto px-1">
-                                <a class="btn btn-outline-light w-100" href="javascript:;">
+                                <!--<a class="btn btn-outline-light w-100" href="javascript:;">
                                     <svg width="24px" height="32px" viewBox="0 0 64 64" version="1.1">
                                         <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
                                             <g transform="translate(3.000000, 3.000000)" fill-rule="nonzero">
@@ -36,13 +34,13 @@
                                             </g>
                                         </g>
                                     </svg>
-                                </a>
+                                </a>!-->
                             </div>
                             <div class="mt-2 position-relative text-center">
-                                <p
+                                <!--<p
                                     class="text-sm font-weight-bold mb-2 text-secondary text-border d-inline z-index-2 bg-white px-3">
                                     or
-                                </p>
+                                </p>!-->
                             </div>
                         </div>
                         <div class="card-body">
