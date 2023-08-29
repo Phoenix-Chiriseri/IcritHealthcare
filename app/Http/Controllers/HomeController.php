bab0cfd5df8fb73cd1036e8614e79ada82ba5e01
itai chiriseri <itaineilchiriseri@gmail.com>
2023-08-29 12:28:04 +0200

    ordered the records on the dashboard desc

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -16,26 +16,31 @@ class HomeController extends Controller
     public function index()
     {
         $currentDate = Carbon::now('Europe/London')->format('d-m-Y');
-        $userId = Auth::id();
-        $username = Auth::user()->username;
-        $house = Auth::user()->house;
-        $numberOfPatientsInHouse = DB::select("SELECT COUNT(*) AS count FROM patients WHERE house = ?", [$house]);        
-        $query = "
-        SELECT users.id AS user_id, users.username AS user_name, users.house AS house,
-        patients.patient_name, daily_entries.id as entryId, daily_entries.date, daily_entries.shift,
-        daily_entries.personal_care, daily_entries.medication_admin,
-        daily_entries.appointments, daily_entries.activities, daily_entries.incident
-        FROM daily_entries
-        LEFT JOIN patients ON daily_entries.patient_id = patients.id
-        LEFT JOIN users ON patients.Staff_Id = users.id
-        WHERE EXISTS (
-            SELECT 1
-            FROM patients AS p
-            WHERE p.Staff_id = :userId
-            AND p.id = daily_entries.patient_id
-        )
-        ";
-        $entries = DB::select($query, ['userId' => $userId]);
-        return view('pages.dashboard', compact('entries'))->with("name", $username)->with("house", $house)->with("numberOfPatients",$numberOfPatientsInHouse)->with("currentDate",$currentDate);    
-  }
+$userId = Auth::id();
+$username = Auth::user()->username;
+$house = Auth::user()->house;
+$numberOfPatientsInHouse = DB::select("SELECT COUNT(*) AS count FROM patients WHERE house = ?", [$house]);
+
+$query = "
+    SELECT users.id AS user_id, users.username AS user_name, users.house AS house,
+    patients.patient_name, daily_entries.id as entryId, daily_entries.date, daily_entries.shift,
+    daily_entries.personal_care, daily_entries.medication_admin,
+    daily_entries.appointments, daily_entries.activities, daily_entries.incident
+    FROM daily_entries
+    LEFT JOIN patients ON daily_entries.patient_id = patients.id
+    LEFT JOIN users ON patients.Staff_Id = users.id
+    WHERE EXISTS (
+        SELECT 1
+        FROM patients AS p
+        WHERE p.Staff_id = :userId
+        AND p.id = daily_entries.patient_id
+    )
+    ORDER BY daily_entries.date DESC
+";
+
+$entries = DB::select($query, ['userId' => $userId]);
+return view('pages.dashboard', compact('entries'))->with("name", $username)->with("house", $house)->with("numberOfPatients",$numberOfPatientsInHouse)->with("currentDate",$currentDate);    
+
+}
+
 }
