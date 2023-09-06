6a438a80c573ceb0820c7f401dd320028f55b99e
itai chiriseri <itaineilchiriseri@gmail.com>
2023-09-06 11:38:43 +0200

    added a jumbotron to the application for the home route

--- resources/views/pages/splashScreen.blade.php
+++ resources/views/pages/splashScreen.blade.php
@@ -10,9 +10,49 @@
     
     <!-- splash-screen CSS from CDN -->
     <link href="https://cdn.jsdelivr.net/npm/splash-screen@4.0.1/dist/splash-screen.min.css" rel="stylesheet">
+
+    <!-- Custom CSS for styling -->
+    <style>
+        body {
+            background-color: #f4f5f7;
+        }
+
+        .splash-container {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh; /* Vertically center the content */
+        }
+
+        /* Customize the video container */
+        #splash-video {
+            width: 100%; /* Make the video responsive */
+            max-width: 800px; /* Adjust the max-width as needed */
+            border-radius: 10px; /* Rounded corners */
+            box-shadow: 0 0 20px rgba(0, 0, 0, 0.3); /* Add a shadow */
+        }
+
+        /* Customize the jumbotron */
+        .custom-jumbotron {
+            background-color: #007BFF;
+            color: #fff;
+            text-align: center;
+            border-radius: 10px;
+            padding: 20px;
+        }
+    </style>
 </head>
 <body>
-    <div class="splash-container">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 offset-md-3">
+                <div class="custom-jumbotron">
+                    <h1>Welcome To B-E Care Management System</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="splash-container" style="margin-right: -100px">
         <video autoplay muted loop id="splash-video">
             <source src="{{ asset('videos/be.mp4') }}" type="video/mp4">
         </video>
@@ -36,5 +76,8 @@
             window.location.href = "{{ route('login') }}";
         }, 11000); // 11,000 milliseconds = 11 seconds
     </script>
+    
+    <!-- Jumbotron with a label -->
+    
 </body>
 </html>
