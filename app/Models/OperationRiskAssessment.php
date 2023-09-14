7307bbbad55d3e49bd8afe9500d21d09e7882c0e
itai chiriseri <itaineilchiriseri@gmail.com>
2023-09-14 03:25:44 +0200

    added the operation risk assessment migration

--- app/Models/OperationRiskAssessment.php
+++ app/Models/OperationRiskAssessment.php
@@ -10,14 +10,44 @@ class OperationRiskAssessment extends Model
     use HasFactory;
 
     protected $fillable = [
+        'assessment_date',
+        'accessors_email_1', // You have two fields with the same name 'accessors_email', so I added '_1' and '_2'
+        'accessors_email_2', // Added '_2' for the second 'accessors_email' field
         'patient_id',
-        'date',
-        'shift',
-        'personal_care',
-        'medication_admin',
-        'appointments',
-        'activities',
-        'incident',
+        'what_causes_harm',
+        'where_is_the_hazard',
+        'who_might_be_harmed',
+        'how_will_they_be_harmed',
+        'how_often_are_exposed_hazard',
+        'how_long_exposed_hazard',
+        'disallowing_activity',
+        'comment',
+        'likelihood_harm',
+        'how_serious_harm',
+        'list_of_control_measures',
+        'date_when_control_measures_implemented',
+        'identity_training_required_risk',
+        'identity_training_was_specified',
+        'identity_equipment_reduced_risk',
+        'date_equipment_provided', // Renamed to 'date_equipment_provided'
+        'likelihood_radio_harm',
+        'how_serious_harm_radio',
+        'additional_control_measures',
+        'completion_control_measures',
+        'risk_assessment_drawn_by',
+        'risk_assessment_date',
+        'assessment_taken_mental',
+        'please_comment_any_behaviours',
+        'positive_liberty_issue',
+        'outcome_best_interest_radio',
+        'comment',
+        'date_of_review',
+        'changes_required',
+        'managers_name',
+        'risk_assessment_Activity_accessed',
+        'date_of_assessment',
+        'signage_name',
+        'signage_date',
     ];
 
     public function user()
--- database/migrations/2023_08_16_113732_create_operation_risk_assessments_table.php
+++ database/migrations/2023_08_16_113732_create_operation_risk_assessments_table.php
@@ -15,16 +15,47 @@ return new class extends Migration
             $table->id();
             $table->unsignedBigInteger('patient_id');
             $table->unsignedBigInteger('user_id');
-            $table->date('date');
-            $table->string('shift');
-            $table->string('personal_care');
-            $table->string('medication_admin');
-            $table->string('appointments');
-            $table->string('activities');
-            $table->string('incident');
-            $table->timestamps();
+            $table->date('assessment_date');
+            $table->string('accessors_email_1'); // Adjusted field name
+            $table->string('accessors_email_2'); // Adjusted field name
+            $table->unsignedBigInteger('patient_id');
+            $table->text('what_causes_harm');
+            $table->text('where_is_the_hazard');
+            $table->text('who_might_be_harmed');
+            $table->text('how_will_they_be_harmed');
+            $table->text('how_often_are_exposed_hazard');
+            $table->text('how_long_exposed_hazard');
+            $table->string('disallowing_activity');
+            $table->text('comment');
+            $table->string('likelihood_harm');
+            $table->string('how_serious_harm');
+            $table->text('list_of_control_measures');
+            $table->date('date_when_control_measures_implemented');
+            $table->text('identity_training_required_risk');
+            $table->date('identity_training_was_specified');
+            $table->text('identity_equipment_reduced_risk');
+            $table->date('date_equipment_provided');
+            $table->string('likelihood_radio_harm');
+            $table->string('how_serious_harm_radio');
+            $table->text('additional_control_measures');
+            $table->date('completion_control_measures');
+            $table->text('risk_assessment_drawn_by');
+            $table->date('risk_assessment_date');
+            $table->string('assessment_taken_mental');
+            $table->text('please_comment_any_behaviours');
+            $table->string('positive_liberty_issue');
+            $table->string('outcome_best_interest_radio');
+            $table->text('comment');
+            $table->date('date_of_review');
+            $table->text('changes_required');
+            $table->text('managers_name');
+            $table->text('risk_assessment_Activity_accessed');
+            $table->date('date_of_assessment');
+            $table->text('signage_name');
+            $table->date('signage_date');
             $table->foreign('patient_id')->references('id')->on('patients');
             $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
         });
     }
 
