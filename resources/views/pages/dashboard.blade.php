a3d8b94275b66f6a778ace9a6f104032c1c2f6d8
itai chiriseri <itaineilchiriseri@gmail.com>
2023-09-16 12:32:13 +0200

    changed the dashboard to show only my daily entry records

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -155,7 +155,7 @@ function generate() {
         <div class="row">
             <div class="col-md-12">
                 <div class="card">
-                    <div class="card-header text-center" style="size: 22px;">Daily Entries</div>
+                    <div class="card-header text-center" style="size: 22px;">My Daily Entry Records</div>
                     <div class="card-body">
                         <div class="table-responsive">
                             <table class="table table-striped table-bordered" id = "DOMContentLoaded">
