7fdb4dfed7d87aedcd32e3b6b655a4e95eb44ca3
itai chiriseri <itaineilchiriseri@gmail.com>
2023-09-16 12:54:58 +0200

    pushed the margin for the table downwards

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -189,7 +189,7 @@ function generate() {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/phpYC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <div class="container">
     <a class="navbar-brand" class = "btn btn-info"></a>
-        <div class="row">
+        <div class="row" style="margin-top: 200px;">
             <div class="col-md-12">
                 <div class="card">
                     <div class="card-header text-center" style="size: 22px;margin-top:100pxk">My Daily Entry Records</div>
